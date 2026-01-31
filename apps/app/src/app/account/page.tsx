c057f3f08505e90e13b1d06777a63631af826982
Lucky Ivanius <luckzivanius@gmail.com>
2026-02-01 00:39:03 +0700

    feat: add empty placeholder pages

---
+++ apps/app/src/app/account/page.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const AccountPage: NextPage = () => {
+  return <div>Account</div>;
+};
+
+export default AccountPage;
---
+++ apps/app/src/app/history/page.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const HistoryPage: NextPage = () => {
+  return <div>History</div>;
+};
+
+export default HistoryPage;
